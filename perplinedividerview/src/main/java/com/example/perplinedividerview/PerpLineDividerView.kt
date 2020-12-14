964b467752a3fd9280e0c60e1b2c45feb07f9a37
Anwesh43 <anweshthecool0@gmail.com>
2020-12-14 13:22:28 +0530

    adding Canvas extension methods

--- perplinedividerview/src/main/java/com/example/perplinedividerview/PerpLineDividerView.kt
+++ perplinedividerview/src/main/java/com/example/perplinedividerview/PerpLineDividerView.kt
@@ -29,3 +29,26 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 
+fun Canvas.drawPerpLineDivider(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val lSize : Float = (2 * size) / (kParts - 1)
+    save()
+    translate(w / 2, h / 2)
+    drawLine(-size + 2 * size * sc3, 0f, -size + 2 * size * sc1, 0f, paint)
+    for (j in 0..(kParts - 1)) {
+        save()
+        translate(lSize * j, 0f)
+        drawLine(0f, 0f, 0f, -lSize * sc2.divideScale(0, parts), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawPLDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    drawPerpLineDivider(scale, w, h, paint)
+}
