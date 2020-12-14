57950666c3a3304279ad67b40875cd8f7c99d2a8
Anwesh43 <anweshthecool0@gmail.com>
2020-12-14 13:40:46 +0530

    adding PLDNode class

--- perplinedividerview/src/main/java/com/example/perplinedividerview/PerpLineDividerView.kt
+++ perplinedividerview/src/main/java/com/example/perplinedividerview/PerpLineDividerView.kt
@@ -109,4 +109,45 @@ class PerpLineDividerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PLDNode(var i : Int, val state : State = State()) {
+
+        private var next : PLDNode? = null
+        private var prev : PLDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PLDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPLDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit ){
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PLDNode {
+            var curr : PLDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
